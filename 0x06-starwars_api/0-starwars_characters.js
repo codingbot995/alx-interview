556e98ec174e5d79963b0c085b2d6e6984ea188b
Jane mathenge <waguthijane61@gmail.com>
2024-07-11 12:03:15 +0000

    api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,28 @@
+
 #!/usr/bin/node
-
 const request = require('request');
+const process = require('process');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map(charUrl => {
+    return new Promise((resolve, reject) => {
+      request(charUrl, (error, response, body) => {
+        if (error) reject(error);
+        else {
+          const characterName = JSON.parse(body).name;
+          resolve(characterName);
+        }
+      });
+    });
   });
-};
+  Promise.all(promises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    });
+});
+
