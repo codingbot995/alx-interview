eeb05df41809361c905d861e91992596c87d7d21
Jane mathenge <waguthijane61@gmail.com>
2024-07-12 06:50:04 +0000

    api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,51 @@
-#!/usr/bin/node
 
+#!/usr/bin/node
+/**
+* Stars Wars Characters
+*/
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+const id = process.argv[2];
+const endpoint = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
+function getCharacter (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Failed to fetch character: ${response.statusCode}`));
+      } else {
+        const data = JSON.parse(body);
+        resolve(data.name);
+      }
+    });
   });
-};
+}
+
+request(endpoint, async (err, response, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch film:', response.statusCode);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characterUrls = filmData.characters;
+
+  if (characterUrls) {
+    for (const url of characterUrls) {
+      try {
+        const name = await getCharacter(url);
+        console.log(name);
+      } catch (error) {
+        console.error('Error fetching character:', error);
+      }
+    }
+  }
+});
+
